b21b4886681433dc08f0b21996e80dbb1562db55
flyingnobita <46126470+flyingnobita@users.noreply.github.com>
2023-01-05 01:47:09 +0800

    Improve performance measure in App.tsx

--- test-client/src/App.tsx
+++ test-client/src/App.tsx
@@ -1,30 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { wrap } from 'comlink';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { wrap } from "comlink";
 
 function App() {
-  const worker = new Worker(new URL('./halo-worker', import.meta.url), {
-    name: 'halo-worker',
-    type: 'module',
+  const worker = new Worker(new URL("./halo-worker", import.meta.url), {
+    name: "halo-worker",
+    type: "module",
   });
-  const workerApi = wrap<import('./halo-worker').HaloWorker>(worker);
-  const [ans, setAns] = useState(0);
+  const workerApi = wrap<import("./halo-worker").HaloWorker>(worker);
+  // const [ans, setAns] = useState(0);
 
   async function test() {
+    console.log(
+      "navigator.hardwareConcurrency: ",
+      navigator.hardwareConcurrency
+    );
+
     const start = performance.now();
     const diff_js = await workerApi.get_play_diff();
-    console.log('in between', diff_js);
-    console.log('btw', performance.now() - start);
+    console.log("in between", diff_js);
+    const t_get_play_diff = performance.now() - start;
+    console.log("t_get_play_diff: ", t_get_play_diff);
     const proof = await workerApi.prove_play();
-    console.log('ending', performance.now() - start);
-    console.log('outside proof', proof);
-
+    const t_prove_play = performance.now() - t_get_play_diff;
+    console.log("t_prove_play: ", t_prove_play);
+    console.log("outside proof", proof);
     const verification = await workerApi.verify_play(proof, diff_js);
-    console.log('verified', verification);
-    console.log('time', performance.now() - start);
+    const t_verify_play = performance.now() - t_prove_play;
+    console.log("t_prove_play: ", t_verify_play);
+    console.log("verified", verification);
   }
-  
 
   return (
     <div className="App">
@@ -33,7 +39,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        {ans}
+        {/* {ans} */}
         <button onClick={test}>test</button>
         <a
           className="App-link"
